53c7cdec6cbfab042dbc6458b721225fc4f8dcc7
Hans <94590267+HansHans135@users.noreply.github.com>
2023-04-06 15:57:19 +0800

    Add files via upload

--- examples/bot.py
+++ examples/bot.py
@@ -3,9 +3,12 @@ import discord
 import json
 from discord.ext import commands
 from discord import option
+from pteropy import Pterodactyl_Client
 
+base_url = "https://面板網址"
 bot=discord.Bot()
-class power(discord.ui.View):
+
+class Power(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
@@ -34,3 +37,36 @@ class power(discord.ui.View):
     )
     async def kill(self, button: discord.ui.Button, interaction: discord.Interaction):
         pass
+
+@bot.slash_command(description="綁定api")
+@option("key", description="你的api key")
+async def key(ctx,key:str):
+    ptero = Pterodactyl_Client(base_url, key)
+    try:
+        ptero.list_servers()["errors"]
+        await ctx.respond("無效的kpi key 請重新嘗試")
+        return
+    except:
+        with open("key.json","r")as f:
+            data=json.load(f)
+        data[ctx.user.id]=key
+        with open("key.json","w+")as f:
+            json.dump(data,f)
+        await ctx.respond("綁定成功")
+
+@bot.slash_command(description="控制電源")
+@option("server", description="你的主機編號")
+async def power(ctx,server:str):
+    with open("key.json","r")as f:
+        data=json.load(f)
+    try:
+        ptero = Pterodactyl_Client(base_url, data[ctx.user.id])
+    except:
+        await ctx.respond("你還沒綁定")
+        return
+    if ptero.get_server(server) == None:
+        await ctx.respond("沒有這個伺服器")
+        return
+    embed=discord.Embed(title="電源選項",description="正在控制"+server)
+    await ctx.respond(content=server,embed=embed, view=Power())
+bot.run("")
